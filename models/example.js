93ead69a0baa9403ddf2ddea8b6c2790dfc0860d
Curtis Charles <ccharles2388@gmail.com>
2019-11-18 16:27:17 -0500

    Fixed The example reference point

--- models/example.js
+++ models/example.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
+  var Reminder = sequelize.define("Reminder", {
     text: DataTypes.STRING,
     description: DataTypes.TEXT
   });
-  return Example;
+  return Reminder;
 };
