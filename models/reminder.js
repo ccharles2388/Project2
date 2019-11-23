1331c3aa4c5b33cde6356268b78d3a72eee468ee
Curtis Charles <ccharles2388@gmail.com>
2019-11-23 11:45:49 -0500

    Deleted Logic Fir User

--- models/reminder.js
+++ models/reminder.js
@@ -6,15 +6,15 @@ module.exports = function (sequelize, DataTypes) {
     alarmType: DataTypes.TEXT
   });
 
-  // Reminder.associate = function (models) {
-  //   // We're saying that a Reminder should belong to an Author
-  //   // A Reminder can't be created without an Author due to the foreign key constraint
-  //   Reminder.belongsTo(models.infoUser, {
-  //     foreignKey: {
-  //       allowNull: false
-  //     }
-  //   });
-  // };
+  Reminder.associate = function (models) {
+    // We're saying that a Reminder should belong to an Author
+    // A Reminder can't be created without an Author due to the foreign key constraint
+    Reminder.belongsTo(models.UserInfo, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
 
   return Reminder;
 
